20b7a645074d0902a37d6f12a7416675d40878e4
Dz1bay <anton.balibin@gmail.com>
2024-11-14 22:02:39 +0300

    code review

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from flask_mysqldb import MySQL
 import json
 import bcrypt
@@ -18,26 +18,35 @@ mysql = MySQL(app)
 def login():
     cur = mysql.connection.cursor()
     email, password = request.json['email'], request.json['password']
+    cur.execute(f"""SELECT password FROM persons where email = '{email}'""")
+    database_response = cur.fetchall()
     try:
-        cur.execute(f"""SELECT password FROM persons where email = '{email}'""")
-        rv = cur.fetchall()[0][0]
-        result = bcrypt.checkpw(password.encode(), rv.encode())
-        return json.dumps(result)
+        user_password = database_response[0][0]
+        is_valid_password = bcrypt.checkpw(password.encode(), user_password.encode())
+        if is_valid_password:
+            return Response('Success', status=200, mimetype='text/xml')
+        else:
+            return Response('Invalid credentials', status=404, mimetype='text/xml')
     except:
-        return json.dumps(False)
+        return Response('Invalid credentials', status=500, mimetype='text/xml')
 
 
 @app.route("/register", methods=['POST'])
 def register():
-    email, password = request.json['email'], request.json['password']
-    hash_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
     cur = mysql.connection.cursor()
-    cur.execute(f"""INSERT INTO `database`.`persons` (`Email`, `Password`) VALUES ("{email}", "{hash_password}");""")
-    mysql.connection.commit()
-    return 'Succes'
+    email, password = request.json['email'], request.json['password']
+    count_users_with_this_email = cur.execute(f"""SELECT * FROM `database`.`persons` WHERE email = '{email}'""")
+    if count_users_with_this_email == 0:
+        hash_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+        cur.execute(f"""INSERT INTO `database`.`persons` (`Email`, `Password`) VALUES ("{email}", "{hash_password}");""")
+        mysql.connection.commit()
+        return Response('Register successfully', status=200, mimetype='text/xml')
+    else:
+        return Response('User with this email is already exists', status=500, mimetype='text/xml')
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 
